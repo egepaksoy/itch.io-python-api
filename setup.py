70bfc4311c10b5ddc0d31e71a5bea9bee03f2e6a
Ege Paksoy <egepaskoy@gmail.com>
2021-04-23 00:37:58 +0300

    name was changed

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "itch.py",
+    name = "itch.io api",
     version = "1.2",
     author = "Ege Paksoy",
     author_email = "egepaskoy@gmail.com",
