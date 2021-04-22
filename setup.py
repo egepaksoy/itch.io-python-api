17753d2f84f3ecb8dd60cd58c2fd49597bbdc964
Ege Paksoy <egepaskoy@gmail.com>
2021-04-23 00:36:45 +0300

    version 1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "itch.py",
-    version = "1.1",
+    version = "1.2",
     author = "Ege Paksoy",
     author_email = "egepaskoy@gmail.com",
     packages = find_packages(),
