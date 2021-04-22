a8cebd852d7ea4aff2a7a035e61ecb5f7513408f
Ege Paksoy <egepaskoy@gmail.com>
2021-04-23 00:33:13 +0300

    created a git point

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "itch.py",
+    version = "1.1",
+    author = "Ege Paksoy",
+    author_email = "egepaskoy@gmail.com",
+    packages = find_packages(),
+    license_files = ('LICENSE.txt')
+)
